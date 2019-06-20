e59dd2b867ed9d6a013458ca49bd47e1bc36b093
saraoswald <sara.linsley@gmail.com>
2019-06-20 18:06:54 -0400

    more content

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-function toggleNav(event){
+function toggleNav(event) {
     $('.no_content').hide();
 
     const $target = $(event.target);
-    if($target.hasClass('active')) {
+    if ($target.hasClass('active')) {
         doHide();
         $target.removeClass('active');
-    }
-    else {
+    } else {
         $('.nav_button').removeClass('active');
         $target.addClass('active');
 
@@ -25,43 +24,47 @@ function doShow($section) {
     $('.nav').addClass('active');
 }
 
-function closeOverlay(){
+function closeOverlay() {
     $('body').off('keydown.overlay');
     $('.close-overlay').off('click tap');
     $('.overlay').removeClass('active').off('click tap');
     $('.overlay-content').empty();
 }
 
-function openOverlay(){
+function openOverlay() {
     const $overlay = $('.overlay');
     // click outside the overlay
+
+    // if($(event.target).hasClass('overlay-wide')) 
     $overlay.addClass('active')
         .off('click tap').on('click tap', e => {
-            if($(e.target).is('.overlay')) closeOverlay();
+            if ($(e.target).is('.overlay')) closeOverlay();
         });
     // press Esc
     $('body').off('keydown.overlay').on('keydown.overlay', e => {
-            if(e.which == 27) closeOverlay()
-        });
+        if (e.which == 27) closeOverlay()
+    });
     // click close button
-    $('.close-overlay').off('click tap').on('click tap', 
+    $('.close-overlay').off('click tap').on('click tap',
         closeOverlay);
 }
 
-function moveOverlay(){
+function moveOverlay() {
     const $overlay = $('.overlay > div');
     const bottom = $('body').height() - window.outerHeight - $overlay.outerHeight();
     const newCoord = window.scrollY;
-    const yCoord = newCoord > bottom
-        ? bottom
-        : newCoord;
+    const yCoord = newCoord > bottom ?
+        bottom :
+        newCoord;
 
     $overlay.css('top', yCoord);
 
 }
 
 function handleOverlayClick(event) {
-    $('.overlay-content').append($(event.target).clone().removeClass('has-overlay'));
+    const $newImg = $(event.target).clone().removeClass('has-overlay');
+    $('.overlay > div').toggleClass('wide', $newImg.hasClass('wide'));
+    $('.overlay-content').append($newImg);
     openOverlay();
     moveOverlay();
 }
